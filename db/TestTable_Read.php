31f825b315b3c3f16ef5a4950ce65d44910e7e35
David BAEK <baek.david@gmail.com>
2020-06-11 17:53:27 +0200

    add db

---
+++ db/TestTable_Read.php
@@ -0,0 +1,35 @@
+﻿<?php
+    
+    $db_host = "localhost";
+    $db_user = "baekdavid";
+    $db_passwd = "r3a9m2s*";
+    $db_name = "baekdavid";
+
+    // MySQL - DB
+    $conn = mysqli_connect($db_host,$db_user,$db_passwd,$db_name);
+    if (mysqli_connect_errno()){
+        echo "MySQL connection failed: " . mysqli_connect_error();
+        exit;
+    }
+
+    // utf8.
+    mysqli_set_charset($conn,"utf8"); 
+
+    // sql
+    $sql = "SELECT * FROM discord_messages";
+    if ($result = mysqli_query($conn,$sql)){
+        $rows = array();
+        while($row = mysqli_fetch_array($result)) {
+            $rows[] = $row;
+        } 
+        $qryResult = array();
+        $qryResult['discord_messages'] = $rows;
+        echo json_encode($qryResult);
+
+        mysqli_close($conn);
+
+    } else {
+        echo "sql error: " . mysqli_error($conn);
+        exit;
+    }
+?>
---
+++ index.js
@@ -0,0 +1,319 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.argv.length == 2 ? process.env.token : "";
+const moment = require("moment");
+require("moment-duration-format");
+const momenttz = require('moment-timezone');
+const MessageAdd = require('./db/message_add.js')
+const welcomeChannelName = "Hello";
+const byeChannelName = "Good bye";
+const welcomeChannelComment = "Welcome";
+const byeChannelComment = "bye bye";
+
+client.on('ready', () => {
+  console.log('Online');
+  client.user.setPresence({ game: { name: 'Type !help' }, status: 'online' })
+});
+
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  const newUser = member.user;
+  const welcomeChannel = guild.channels.find(channel => channel.name == welcomeChannelName);
+
+  welcomeChannel.send(`<@${newUser.id}> ${welcomeChannelComment}\n`);
+
+  member.addRole(guild.roles.find(role => role.name == "Guest"));
+});
+
+client.on("guildMemberRemove", (member) => {
+  const guild = member.guild;
+  const deleteUser = member.user;
+  const byeChannel = guild.channels.find(channel => channel.name == byeChannelName);
+
+  byeChannel.send(`<@${deleteUser.id}> ${byeChannelComment}\n`);
+});
+
+client.on("messageUpdate", (message) => {
+  MessageSave(message, true)
+});
+
+client.on('message', (message) => {
+  MessageSave(message)
+  if(message.author.bot) return;
+
+  if(message.content == 'ping') {
+    return message.reply('pong');
+  }
+
+  if(message.content == '!si') {
+    let embed = new Discord.RichEmbed()
+    let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
+    var duration = moment.duration(client.uptime).format(" D, H, m, s");
+    embed.setColor('#186de6')
+    embed.setAuthor('server info of cokebear BOT', img)
+    embed.setFooter(`coke BOT ❤️`)
+    embed.addBlankField()
+    embed.addField('RAM usage',    `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true);
+    embed.addField('running time', `${duration}`, true);
+    embed.addField('user',         `${client.users.size.toLocaleString()}`, true);
+    embed.addField('server',       `${client.guilds.size.toLocaleString()}`, true);
+    // embed.addField('channel',      `${client.channels.size.toLocaleString()}`, true);
+    embed.addField('Discord.js',   `v${Discord.version}`, true);
+    embed.addField('Node',         `${process.version}`, true);
+    
+    let arr = client.guilds.array();
+    let list = '';
+    list = `\`\`\`css\n`;
+    
+    for(let i=0;i<arr.length;i++) {
+      // list += `${arr[i].name} - ${arr[i].id}\n`
+      list += `${arr[i].name}\n`
+    }
+    list += `\`\`\`\n`
+    embed.addField('list:',        `${list}`);
+
+    embed.setTimestamp()
+    message.channel.send(embed);
+  }
+
+  if(message.content == 'embed') {
+    let img = 'https://cdn.discordapp.com/icons/419671192857739264/6dccc22df4cb0051b50548627f36c09b.webp?size=256';
+    let embed = new Discord.RichEmbed()
+      .setTitle('Title')
+      .setThumbnail(img)
+      .addBlankField()
+      .addField('Inline field title', 'Some value here')
+      .addField('Inline field title', 'Some value here', true)
+      .addField('Inline field title', 'Some value here', true)
+      .addField('Inline field title', 'Some value here', true)
+      .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
+      .addBlankField()
+      .setTimestamp()
+    
+    message.channel.send(embed)
+  } else if(message.content == '!help') {
+    let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
+    let commandList = [
+      {name: '!help', desc: 'help'},
+      {name: 'ping', desc: 'ping health'},
+      {name: 'embed', desc: 'embed Ex1'},
+      {name: '!Notice', desc: 'Notice by dm'},
+      {name: '!Notice2', desc: 'Notice by embbed dm'},
+      {name: '!Clean up', desc: 'Clean message up'},
+      {name: '!Invite', desc: 'Invitation code from a channel'},
+      {name: '!Invite2', desc: 'Inviation code frome all channel'},
+    ];
+    let commandStr = '';
+    let embed = new Discord.RichEmbed()
+      .setAuthor('Help of cokebear BOT', helpImg)
+      .setColor('#186de6')
+      .setFooter(`cokebear BOT ❤️`)
+      .setTimestamp()
+    
+    commandList.forEach(x => {
+      commandStr += `• \`\`${changeCommandStringLength(`${x.name}`)}\`\` : **${x.desc}**\n`;
+    });
+
+    embed.addField('Commands: ', commandStr);
+
+    message.channel.send(embed)
+  } else if(message.content == '!Invite2') {
+    client.guilds.array().forEach(x => {
+      x.channels.find(x => x.type == 'text').createInvite({maxAge: 0}) // 24 hours without maxAge: 0
+        .then(invite => {
+          message.channel.send(invite.url)
+        })
+        .catch((err) => {
+          if(err.code == 50013) {
+            message.channel.send('**'+x.channels.find(x => x.type == 'text').guild.name+'** Admin Permission needed')
+          }
+        })
+    });
+  } else if(message.content == '!Invite') {
+    if(message.channel.type == 'dm') {
+      return message.reply('Not allowed Command by dm');
+    }
+    message.guild.channels.get(message.channel.id).createInvite({maxAge: 0}) 
+      .then(invite => {
+        message.channel.send(invite.url)
+      })
+      .catch((err) => {
+        if(err.code == 50013) {
+          message.channel.send('**'+message.guild.channels.get(message.channel.id).guild.name+'**')
+        }
+      })
+  } else if(message.content.startsWith('!Invite2')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { // Notice from the channel
+      let contents = message.content.slice('!Notice2'.length);
+      let embed = new Discord.RichEmbed()
+        .setAuthor('Notice of cokebear BOT')
+        .setColor('#186de6')
+        .setFooter(`cokebear BOT ❤️`)
+        .setTimestamp()
+  
+      embed.addField('Notice: ', contents);
+  
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(embed)
+      });
+  
+      return message.reply('Notice sent');
+    } else {
+      return message.reply('Go Channel');
+    }
+  } else if(message.content.startsWith('!Notice')) {
+    if(checkPermission(message)) return
+    if(message.member != null) { 
+      let contents = message.content.slice('!Notice'.length);
+      message.member.guild.members.array().forEach(x => {
+        if(x.user.bot) return;
+        x.user.send(`<@${message.author.id}> ${contents}`);
+      });
+  
+      return message.reply('Notice sent');
+    } else {
+      return message.reply('Go Channel');
+    }
+  } else if(message.content.startsWith('!Clean Up')) {
+    if(message.channel.type == 'dm') {
+      return message.reply('Not allowed command by dm');
+    }
+    
+    if(message.channel.type != 'dm' && checkPermission(message)) return
+
+    var clearLine = message.content.slice('!Clean Up'.length);
+    var isNum = !isNaN(clearLine)
+
+    if(isNum && (clearLine <= 0 || 100 < clearLine)) {
+      message.channel.send("Input number from 1 to 100")
+      return;
+    } else if(!isNum) { 
+      if(message.content.split('<@').length == 2) {
+        if(isNaN(message.content.split(' ')[2])) return;
+
+        var user = message.content.split(' ')[1].split('<@!')[1].split('>')[0];
+        var count = parseInt(message.content.split(' ')[2])+1;
+        let _cnt = 0;
+
+        message.channel.fetchMessages().then(collected => {
+          collected.every(msg => {
+            if(msg.author.id == user) {
+              msg.delete();
+              ++_cnt;
+            }
+            return !(_cnt == count);
+          });
+        });
+      }
+    } else {
+      message.channel.bulkDelete(parseInt(clearLine)+1)
+        .then(() => {
+          AutoMsgDelete(message, `<@${message.author.id}> ` + parseInt(clearLine) + "messages deleted");
+        })
+        .catch(console.error)
+    }
+  }
+});
+
+function checkPermission(message) {
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) {
+    message.channel.send(`<@${message.author.id}> ` + "Admin permission needed")
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function changeCommandStringLength(str, limitLen = 8) {
+  let tmp = str;
+  limitLen -= tmp.length;
+
+  for(let i=0;i<limitLen;i++) {
+      tmp += ' ';
+  }
+
+  return tmp;
+}
+
+async function AutoMsgDelete(message, str, delay = 3000) {
+  let msg = await message.channel.send(str);
+
+  setTimeout(() => {
+    msg.delete();
+  }, delay);
+}
+
+function getEmbedFields(message, modify=false) {
+  if(message.content == '' && message.embeds.length > 0) {
+    let e = message.embeds[0].fields;
+    let a = [];
+
+    for(let i=0;i<e.length;i++) {
+        a.push(`\`${e[i].name}\` - \`${e[i].value}\`\n`);
+    }
+
+    return a.join('');
+  } else if(modify) {
+    return message.author.lastMessage.content;
+  } else {
+    return message.content;
+  }
+}
+
+function MessageSave(message, modify=false) {
+  imgs = []
+  if (message.attachments.array().length > 0) {
+    message.attachments.array().forEach(x => {
+      imgs.push(x.url+'\n')
+    });
+  }
+
+  username = message.author.username.match(/[\u3131-\uD79D^a-zA-Z^0-9]/ugi)
+  channelName = message.channel.type != 'dm' ? message.channel.name : ''
+  try {
+    username = username.length > 1 ? username.join('') : username
+  } catch (error) {}
+
+  try {
+    channelName = channelName.length > 1 ? channelName.join('') : channelName
+  } catch (error) {}
+
+  var s = {
+    ChannelType: message.channel.type,
+    ChannelId: message.channel.type != 'dm' ? message.channel.id : '',
+    ChannelName: channelName,
+    GuildId: message.channel.type != 'dm' ? message.channel.guild.id : '',
+    GuildName: message.channel.type != 'dm' ? message.channel.guild.name : '',
+    Message: getEmbedFields(message, modify),
+    AuthorId: message.author.id,
+    AuthorUsername: username + '#' + message.author.discriminator,
+    AuthorBot: Number(message.author.bot),
+    Embed: Number(message.embeds.length > 0), // 0 = false
+    
+  }
+
+  s.Message = (modify ? '[modified] ' : '') + imgs.join('') + s.Message
+
+  MessageAdd(
+    s.ChannelType,
+    s.ChannelId,
+    s.ChannelName,
+    s.GuildId,
+    s.GuildName,
+    s.Message,
+    s.AuthorId,
+    s.AuthorUsername,
+    s.AuthorBot,
+    s.Embed,
+    s.CreateTime,
+  )
+    // .then((res) => {
+    //   console.log('saved', res);
+    // })
+    .catch(error => console.log(error))
+}
+
+
+client.login(token);
